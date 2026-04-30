38fb5b65b3b92c5cab7250740c72e5e10325ade7
parasharrupak47 <rupakp.ug23.cs@nitp.ac.in>
2026-04-30 18:33:58 +0530

    added cors

--- server/index.js
+++ server/index.js
@@ -8,7 +8,9 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors(
+  origin = process.env.FRONTEND_URL || "http://localhost:3000"
+));
 app.use(express.json());
 
 // Connect to MongoDB
